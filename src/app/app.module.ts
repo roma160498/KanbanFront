de87dd10871bfa351d144781f6795a7473dc6d8b
Roman Martseniuk <roma160498@yandex.ru>
2018-07-20 23:10:00 +0300

    Created login form with background. Added some test images (should be deleted later). Added material design dependency. Imported some modules for expansion panel.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from '@angular/router';
+
+import {MatFormFieldModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: 'login', component: LoginFormComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
