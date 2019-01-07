2f3c3481287e2f03be118ffbc5d239314deb08d8
Roman Martseniuk <roma160498@yandex.ru>
2019-01-07 12:31:16 +0300

    Update increment service to create relationship between increment and iterations items

--- src/app/services/increment.service.ts
+++ src/app/services/increment.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Increment } from '../models/increment';
+import { DateHelperService } from './date-helper.service';
+import { SequenceHelperService } from './sequence-helper.service';
+import { Iteration } from '../models/iteration';
 
 @Injectable()
 export class IncrementService {
 
-  constructor(private http: HttpClient) { }
-  getIncrement(args): Observable<Increment[]> {
+	constructor(private http: HttpClient, private dateHelper: DateHelperService,
+		private sequenceHelper: SequenceHelperService) { }
+	getIncrement(args): Observable<Increment[]> {
 		return this.http.get('http://localhost:3000/increments', {
 			withCredentials: true, params: {
 				'amount': args.amount,
@@ -17,24 +21,14 @@ export class IncrementService {
 		}).
 			map((response: any[]) => {
 				response.forEach(element => {
-					debugger;
-					const zeroAmount = 6 - element.id.toString().length;
-					let zeroString = '';
-					for (let i = 0; i < zeroAmount; i++) {
-						zeroString += '0';
-					}
-					element.number = 'PI-' + zeroString + element.id;
-					element.end_date = this.getDateFormat((new Date(element.end_date)));
-					element.start_date = this.getDateFormat((new Date(element.start_date)));
+					element.number = this.sequenceHelper.getSequenceFor('PI-', 6, element.id);
+					element.end_date = this.dateHelper.getDateFormat((new Date(element.end_date)));
+					element.start_date = this.dateHelper.getDateFormat((new Date(element.start_date)));
 				});
 				return response
 			}).catch(e => {
 				return Observable.throw(e);
 			});
-	}
-	getDateFormat(dateIn) {
-		return dateIn.toLocaleString('en-us', { year: 'numeric', month: '2-digit', day: '2-digit' }).
-			replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
 	}
 	getIncrementCount(args): Observable<any> {
 		return this.http.get('http://localhost:3000/increments', {
@@ -73,7 +67,7 @@ export class IncrementService {
 			});
 	}
 	updateIncrement(increment: Increment, id) {
-		const body = { increment:increment };
+		const body = { increment: increment };
 		return this.http.put('http://localhost:3000/increments/' + id, body, { withCredentials: true }).
 			map((response: Response) => {
 				if (response.status == 201) {
@@ -81,6 +75,41 @@ export class IncrementService {
 				} else {
 					return null;
 				}
+			}).catch(e => {
+				return Observable.throw(e);
+			});
+	}
+	getIterationsOfIncrementCount(args, incrementId): Observable<Iteration[]> {
+		return this.http.get('http://localhost:3000/increments/' + incrementId + '/iterations', {
+			withCredentials: true, params: {
+				'amount': args.amount,
+				'offset': args.offset,
+				'properties': args.properties,
+				'isCount': 'true'
+			}
+		}).
+			map((response: Response) => {
+				return response
+			}).catch(e => {
+				return Observable.throw(e);
+			});
+	}
+	getIterationsOfIncrement(args, incrementId): Observable<Iteration[]> {
+		return this.http.get('http://localhost:3000/increments/' + incrementId + '/iterations', {
+			withCredentials: true, params: {
+				'amount': args.amount,
+				'offset': args.offset,
+				'properties': args.properties
+			}
+		}).
+			map((response: any[]) => {
+				response.forEach(element => {
+					element.increment_number = this.sequenceHelper.getSequenceFor('PI-', 6, element.increment_id);
+					element.number = this.sequenceHelper.getSequenceFor('IT-', 6, element.id);
+					element.end_date = this.dateHelper.getDateFormat((new Date(element.end_date)));
+					element.start_date = this.dateHelper.getDateFormat((new Date(element.start_date)));
+				});
+				return response
 			}).catch(e => {
 				return Observable.throw(e);
 			});
